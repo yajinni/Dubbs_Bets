b06513c1e5bd97b12dc350cc0bb4cd54a4ad06b1
yajinni <Yajinni@users.noreply.github.com>
2026-06-10 20:35:00 -0400

    Change button text from 'View Odds' to 'View' in Upcoming Fixtures feed

--- src/App.jsx
+++ src/App.jsx
@@ -207,7 +207,7 @@ export default function App() {
                                 setActiveTab('matches');
                               }}
                             >
-                              View Odds
+                              View
                             </button>
                           </div>
                         );
